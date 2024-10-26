2d0a07c206b814996cbebb36e7a6fafd0838b105
Huỳnh Thái Sang <huynhthaisang2011@gmail.com>
2024-10-26 19:19:35 +0700

    update

--- 13.js
+++ 13.js
@@ -1,2 +1,7 @@
 //13. Tạo một hàm nhận vào một mảng số và trả về mảng mới chứa các số lớn hơn 5.
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function number(x) {
+  return x >= 5;
+}
+console.log(number());
