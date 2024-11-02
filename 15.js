5f2de88f84732789a0272f2326ae689c1975fbba
Huỳnh Thái Sang <huynhthaisang2011@gmail.com>
2024-11-02 22:27:11 +0700

    update

--- 15.js
+++ 15.js
@@ -1,7 +1,6 @@
 //Tạo một hàm nhận vào một chuỗi và trả về số lần xuất hiện của một ký tự cụ thể trong chuỗi đó.
 // let document = 'adskfjgaaskjadfghksagdajfjhl'
 
-
 // function checkstring(document, repeat) {
 //     let count = 0;
 //     for (let i = 0; i < document.length; i++) {
@@ -12,21 +11,20 @@
 //     return count
 // }
 
-
 // console.log(checkstring(document,'a'));
 
+const document = "isnvijnvdufvfid";
 
-const a = "kjdfhbkjasdhfkljsadhf";
-function check_String(b) {
-  const c = {};
-  for (let i = 0; i < b.length; i++) {
-    const check = b[i];
-    if (c[check]) {
-      c[check]++;
+function check_string() {
+  let a = {};
+  for (let i = 0; i < document.length; i++) {
+    const check = document[i];
+    if (a[check]) {
+      a[check]++;
     } else {
-      c[check] = 1;
+      a[check] = 1;
     }
   }
-  return c;
+  return a
 }
-console.log(check_String(a));
+console.log(check_string('a'));
