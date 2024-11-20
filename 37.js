b63ac722c22b1bc2ea437f198a0c2442089b54a2
Huỳnh Thái Sang <huynhthaisang2011@gmail.com>
2024-11-20 16:52:55 +0700

    Update 37.js

--- 37.js
+++ 37.js
@@ -1,18 +1,18 @@
 
 // Sử dụng `try...catch` để xử lý lỗi trong một hàm tính toán với dữ liệu không hợp lệ.
 
-function sum(a, b) {
-    try {
-        if (a >= b) {
-            console.log(`a sai`);
-            throw new Error('lỗi a');
-        }
-        console.log(`a đúng`);
-    } catch (error) {
-        console.log(error);
-    }
-}
-console.log(sum(0, 5));
+// function sum(a, b) {
+//     try {
+//         if (a >= b) {
+//             console.log(`a sai`);
+//             throw new Error('lỗi a');
+//         }
+//         console.log(`a đúng`);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+// console.log(sum(0, 5));
 
 
 function sum(a, b) {
@@ -27,21 +27,21 @@ function sum(a, b) {
         console.log(error);
     }
 }
-console.log(sum(0, 5));
+console.log(sum(5, 0));
 
 
 
-function sum(a, b) {
-    if (a < b) {
-        console.log(`a đúng`);
-    } else {
-        console.log(`a sai`);
-        throw new Error('lỗi a');
-    }
-}
+// function sum(a, b) {
+//     if (a < b) {
+//         console.log(`a đúng`);
+//     } else {
+//         console.log(`a sai`);
+//         throw new Error('lỗi a');
+//     }
+// }
 
-try {
-    console.log(sum(0, 5));
-} catch (error) {
-    console.log(error);
-}
+// try {
+//     console.log(sum(0, 5));
+// } catch (error) {
+//     console.log(error);
+//}
